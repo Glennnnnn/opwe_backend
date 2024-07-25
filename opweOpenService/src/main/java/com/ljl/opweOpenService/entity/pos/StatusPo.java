fe7afc38200573a4fbf0aec0482e8ddfacb013d0
glennnnnn <ljlsjcnh649692327@gmail.com>
2024-07-25 21:40:44 +0800

    [250724]--[Glenn]--[data type of long converted to string when serializing]

--- opweOpenService/src/main/java/com/ljl/opweOpenService/entity/pos/StatusPo.java
+++ opweOpenService/src/main/java/com/ljl/opweOpenService/entity/pos/StatusPo.java
@@ -1,5 +1,7 @@
 package com.ljl.opweOpenService.entity.pos;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,6 +18,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class StatusPo {
+    @JsonSerialize(using = ToStringSerializer.class)
     private Long statusId;
     private String statusName;
     private String statusGroup;
